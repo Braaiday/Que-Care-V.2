7c0382f10a4f8b4e8f124684039a36cff2a16f39
BuildTools <unconfigured@null.spigotmc.org>
2021-10-29 11:44:33 +0200

    You can now add Departments though admin page.

--- Que-Care V.2/AddDepartment.aspx
+++ Que-Care V.2/AddDepartment.aspx
@@ -3,5 +3,36 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <br />
+    <center>
+        <div class="card text-white bg-secondary mb-3" style="max-width: 30rem; min-height: 35rem">
+          <div class="card-header">Department</div>
+          <div class="card-body">
+            <h4 class="card-title" style="text-align: center">Add Department</h4>
+              <asp:Label ID="Label1" runat="server" Text="Department"  ></asp:Label>
+              <asp:TextBox ID="txtDepartmentName" runat="server"  CssClass="form-control"></asp:TextBox>
+              <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ForeColor="red" ErrorMessage="Please give the Specilization a name." ControlToValidate="txtDepartmentName"></asp:RequiredFieldValidator>
+              <br />
+              <br />
+              
+              <asp:Label ID="Label2" runat="server" Text="Describe the Department(Optional)"  ></asp:Label>
+              <textarea  id="txtDepartmentFunction" name="txtDepartmentFunction" class="form-control"  rows="3"></textarea>
+              <%--<asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Please describe what the apartment does." ControlToValidate="txtSpecializationFunction"></asp:RequiredFieldValidator>--%>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <asp:Button ID="btnAddDepartment" runat="server" CssClass="btn btn-primary" Text="Add to system." OnClick="btnAddSpecialization_Click" />
+          </div>
+            <div class="card-footer text-right">
+                    &copy; Que-Care
+             </div>
+        </div>
+        
+    </center>
+    <br />
+    <br />
+    <br />
 </asp:Content>
 
--- Que-Care V.2/AddDepartment.aspx.cs
+++ Que-Care V.2/AddDepartment.aspx.cs
@@ -4,11 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class _Default : System.Web.UI.Page
 {
+    int ID;
+    string Dept_Name;
+    string Dept_Function = "Not Described";
     protected void Page_Load(object sender, EventArgs e)
     {
+        ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        string connection = ConfigurationManager.ConnectionStrings["QueCareConnectionString"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connection);
+        conn.Open();
+        string checksize = "select count(*) from Department";
+        SqlCommand size = new SqlCommand(checksize, conn);
+        using (SqlDataReader dr = size.ExecuteReader())
+        {
+            while (dr.Read())
+            {
+                ID = int.Parse(dr[0].ToString()) + 1;
+            }
+        }
+    }
 
+    protected void btnAddSpecialization_Click(object sender, EventArgs e)
+    {
+        string connection = ConfigurationManager.ConnectionStrings["QueCareConnectionString"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connection);
+        conn.Open();
+        Dept_Name = txtDepartmentName.Text;
+        Dept_Function = Request.Form["txtDepartmentFunction"].ToString();
+        if (RequiredFieldValidator1.IsValid == true)
+        {
+            string query = "Insert into Department(Dept_ID,Dept_Name,Dept_Function)Values(@Dept_ID,@Dept_Name,@Dept_Funtion)";
+            SqlCommand command = new SqlCommand(query, conn);
+            command.Parameters.AddWithValue("Dept_ID", ID);
+            command.Parameters.AddWithValue("Dept_Name", Dept_Name);
+            command.Parameters.AddWithValue("Dept_Funtion", Dept_Function);
+            command.ExecuteNonQuery();
+            conn.Close();
+            Server.Transfer("AdminLanding.aspx");
+        }
     }
 }
